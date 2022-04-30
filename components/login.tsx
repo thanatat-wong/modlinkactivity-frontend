b1066936476a70fd448f9f0303b11a443b88dd80
thanatat.wong <thanatat.wong@mail.kmutt.ac.th>
2022-04-30 22:44:28 +0700

    🐯 add: css style

---
+++ components/login.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import React from "react";
+
+const login = () =>{
+    return(
+        <p className="pt-3 text-center text-[24px] font-bold">
+            สวัสดีชาวโลก
+        </p>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
 import '../styles/globals.css'
+import "tailwindcss/tailwind.css";
+
 import type { AppProps } from 'next/app'
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+import Script from "next/script";
+import getConfig from "next/config";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head><title>Activity Recommendation System</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+        />
+        </Head>
+        <Script
+          src="https://kit.fontawesome.com/a7ef51f3aa.js"
+          crossOrigin="anonymous"
+        ></Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+MyApp.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  return { ...appProps };
+};
 
 export default MyApp
